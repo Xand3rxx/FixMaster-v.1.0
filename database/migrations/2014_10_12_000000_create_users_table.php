c762029b9cceb4b22e4477da60fbc61b02d5e358
Adeolu <osunsamiadeolu@gmail.com>
2021-06-21 02:31:05 -0700

    Client service request (#33)
    * Update artisan.php
    
    * Update Handler.php
    
    * Notification Inclusion
    
    Included Notifications
    1. CSE Accepted Job
    2.  CSE ASSIGN Technician
    3. CSE Assign QA
    
    * update on cse & Supplier notification
    
    * Live Server Bug Fix
    
    Errors corrected while we were testing on live server
    
    * Client service request
    
    Client Service Request Correction Part 1
    
    * Service Request Paystack v1
    
    Working Service Request with paystack payment
    
    * changes
    
    * Update FlutterwavePayment.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
 
             $table->id();
             $table->uuid('uuid')->unique();
+            
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
