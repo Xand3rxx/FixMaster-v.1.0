cefdef31df32effed294c8ef4314674337cf2657
Xand3rxx <anthonyjoboy2016@gmail.com>
2021-07-17 16:02:14 +0100

    Forgot password bug fixing

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.auth')
-@section('title', 'Login')
+@section('title', 'Reset Password')
 @section('content')
 
 <section class="bg-home bg-circle-gradiant d-flex align-items-center">
@@ -14,8 +14,8 @@
                         <div class="align-items-center text-center justify-content-center">   
                             <img src="{{ asset('assets/images/home-fix-logo.png')}}" class="img-fluid d-block mx-auto" alt="FixMaster Logo" style="width: 15em; height: 15em; margin-top: -100px !important; margin-bottom: -60px !important;">
                         </div>
-                        <h4 class="card-title text-center">Login</h4>  
-                        <form class="login-form mt-4" method="POST" action="{{ route('login', app()->getLocale()) }}">
+                        <h4 class="card-title text-center">Forgot Password</h4>  
+                        <form class="login-form mt-4" method="POST" action="{{ route('password.update', app()->getLocale()) }}">
                             @csrf
                             <div class="row">
                                 <div class="col-lg-12">
@@ -30,30 +30,37 @@
                                         @enderror
                                     </div>
                                 </div>
+
                                 <div class="col-lg-12">
                                     <div class="form-group position-relative">
                                         <label>Password <span class="text-danger">*</span></label>
                                         <i data-feather="key" class="fea icon-sm icons"></i>
                                         <input type="password" name="password" class="form-control pl-5" placeholder="Password" required>
+                                        <small class="text-muted">Password must be minimum of 6 characters</small>
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
                                     </div>
+                                    
                                 </div>
+
                                 <div class="col-lg-12">
-                                    <div class="d-flex justify-content-between">
-                                        <div class="form-group">
-                                            <div class="custom-control custom-checkbox">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck1" name="remember_me">
-                                                <label class="custom-control-label" for="customCheck1">Remember me</label>
-                                            </div>
-                                        </div>
-                                        <p class="forgot-pass mb-0"><a href="{{ route('password.request', app()->getLocale()) }}" class="text-dark font-weight-bold">Forgot password ?</a></p>
+                                    <div class="form-group position-relative">
+                                        <label>Confirm Password <span class="text-danger">*</span></label>
+                                        <i data-feather="key" class="fea icon-sm icons"></i>
+                                        <input type="password" name="password_confirmation" class="form-control pl-5" placeholder="Password" required>
+                                        @error('password_confirmation')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
                                     </div>
                                 </div>
+
                                 <div class="col-lg-12 mb-0">
-                                    <button type="submit" class="btn btn-primary btn-block">Login</button>
-                                </div>
-                                
-                                <div class="col-12 text-center">
-                                    <p class="mb-0 mt-3"><small class="text-dark mr-2">Don't have an account ?</small> <a href="{{ route('frontend.registration.client.index', app()->getLocale()) }}" class="text-dark font-weight-bold">Register</a></p>
+                                    <button type="submit" class="btn btn-primary btn-block">Reset Password</button>
                                 </div>
                             </div>
                         </form>
