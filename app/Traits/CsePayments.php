16dccc37bcba55c2b89189dd0fa08d1509aa61f0
Isaac Olaolu <isaacolaolu10@gmail.com>
2021-07-31 07:40:55 +0100

    CSE Reoccuring Payment

--- app/Traits/CsePayments.php
+++ app/Traits/CsePayments.php
@@ -5,11 +5,13 @@ namespace App\Traits;
 use App\Models\CollaboratorsPayment;
 use Illuminate\Support\Str;
 
+// Generate re-occurring payment for CSE
+
 trait CsePayments
 {
-    public static function csePayment($userId, $service_request_id, $service_type)
+    public static function csePayment($user_id, $service_request_id, $service_type)
     {
-        return self::createCsePayment($userId, $service_request_id, $service_type);
+        return self::createCsePayment($user_id, $service_request_id, $service_type);
     }
 
     protected static function createCsePayment($user_id, $service_request_id, $service_type)
